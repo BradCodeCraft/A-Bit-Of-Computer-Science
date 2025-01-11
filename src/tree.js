e0f6ff2eb8f22bea90423b4696e18a1fe9a2efe6
bradcodecraft <bradcodecraft@gmail.com>
2025-01-10 19:40:56 -0500

    Tree

--- src/tree.js
+++ src/tree.js
@@ -1,6 +1,6 @@
 class Node {
   constructor(value) {
-    this.value = value || null;
+    this.value = value;
     this.left = null;
     this.right = null;
   }
@@ -75,6 +75,136 @@ class Tree {
     let temp = current.left;
     current.left = null;
     return temp.value;
+  }
+
+  find(value) {
+    return this.#findRec(value, this.root);
+  }
+
+  #findRec(value, tree) {
+    if (!tree) return false;
+
+    if (value < tree.value) {
+      return this.#findRec(value, tree.left);
+    } else if (value > tree.value) {
+      return this.#findRec(value, tree.right);
+    } else {
+      return value === tree.value;
+    }
+  }
+
+  levelOrder(callback) {
+    if (!callback) {
+      throw new Error('Callback is required');
+    }
+
+    const queue = [this.root];
+    const result = [];
+    while (queue.length) {
+      const current = queue.shift();
+      callback(current.value);
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+
+      result.push(current.value);
+    }
+    return result;
+  }
+
+  inOrder(callback) {
+    const arr = []
+    this.#inOrderRec(callback, this.root, arr);
+    return arr;
+  }
+
+  #inOrderRec(callback, tree, arr) {
+    if (!callback) {
+      throw new Error("Callback is required");
+    }
+
+    if (!tree) return;
+    this.#inOrderRec(callback, tree.left, arr);
+    callback(tree.value);
+    arr.push(tree.value);
+    this.#inOrderRec(callback, tree.right, arr);
+  };
+
+  preOrder(callback) {
+    const arr = [];
+    this.#preOrderRec(callback, this.root, arr);
+    return arr;
+  }
+
+  #preOrderRec(callback, tree, arr) {
+    if (!callback) {
+      throw new Error("Callback is required");
+    }
+
+    if (!tree) return;
+    callback(tree.value);
+    arr.push(tree.value);
+    this.#preOrderRec(callback, tree.left, arr);
+    this.#preOrderRec(callback, tree.right, arr);
+  };
+
+  postOrder(callback) {
+    const arr = [];
+    this.#postOrderRec(callback, this.root, arr);
+    return arr;
+  }
+
+  #postOrderRec(callback, tree, arr) {
+    if (!callback) {
+      throw new Error("Callback is required");
+    }
+
+    if (!tree) return;
+    this.#postOrderRec(callback, tree.left, arr);
+    this.#postOrderRec(callback, tree.right, arr);
+    callback(tree.value);
+    arr.push(tree.value);
+  };
+
+  height(node) {
+    if (!node) return 0;
+
+    const leftHeight = this.height(node.left);
+    const rightHeight = this.height(node.right);
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  depth(node) {
+    return this.#depthRec(node, this.root);
+  }
+
+  #depthRec(node, tree) {
+    if (!tree) return -1;
+
+    if (node.value === tree.value) return 0;
+
+    if (node.value < tree.value) {
+      return 1 + this.#depthRec(node, tree.left);
+    } else {
+      return 1 + this.#depthRec(node, tree.right);
+    }
+  }
+
+  isBalanced() {
+    return Math.abs(this.height(this.root.left) - this.height(this.root.right)) <= 1;
+  }
+
+  rebalance() {
+    const arr = [];
+    this.#inOrderRecTraversal(this.root, arr);
+    this.root = this.buildTree(arr);
+  }
+
+  #inOrderRecTraversal(tree, arr) {
+    if (!tree) return;
+    this.#inOrderRecTraversal(tree.left, arr);
+    arr.push(tree.value);
+    this.#inOrderRecTraversal(tree.right, arr);
   }
 }
 
--- test/tree.test.js
+++ test/tree.test.js
@@ -25,4 +25,50 @@ test('delete', () => {
 
   tree.delete(6);
   expect(tree.root.value).toBe(7);
+
+  tree = new Tree([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+});
+
+test('find', () => {
+  expect(tree.find(5)).toBe(true);
+  expect(tree.find(10)).toBe(false);
+})
+
+test('levelOrder', () => {
+  expect(tree.levelOrder(function(node) { return node.value; })).toEqual([5, 3, 8, 2, 4, 7, 9, 1, 6]);
+});
+
+test('inOrder', () => {
+  expect(tree.inOrder(function(node) { return node.value; })).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+});
+
+test('preOrder', () => {
+  expect(tree.preOrder(function(node) { return node.value; })).toEqual([5, 3, 2, 1, 4, 8, 7, 6, 9]);
+});
+
+test('postOrder', () => {
+  expect(tree.postOrder(function(node) { return node.value; })).toEqual([1, 2, 4, 3, 6, 7, 9, 8, 5]);
+});
+
+test('height', () => {
+  expect(tree.height()).toBe(0);
+  expect(tree.height(tree.root)).toBe(4);
+});
+
+test('depth', () => {
+  expect(tree.depth(tree.root)).toBe(0);
+  expect(tree.depth(tree.root.left.left.left)).toBe(3)
+});
+
+test('isBalanced', () => {
+  expect(tree.isBalanced()).toBe(true);
+  tree.insert(10);
+  tree.insert(11);
+  tree.insert(12);
+  expect(tree.isBalanced()).toBe(false);
+});
+
+test('rebalance', () => {
+  tree.rebalance();
+  expect(tree.isBalanced()).toBe(true);
 });
